cbd4232dc75c0e06988c3b1d73af891ffd3ec5ed
Paulo Turino <paulo.turino01@gmail.com>
2023-07-27 13:47:30 -0300

    feat: implementando switcher de temas

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import { GlobalStyle } from "@/styles/GlobalStyles"
 import { CustomThemeProvider } from "@/contexts/ThemeContext";
+import Switcher from "@/components/Switcher";
 
 export default function Home() {
   return (
     <CustomThemeProvider>
       <GlobalStyle />
+      <Switcher />
       <h1>Hello World!</h1>
     </CustomThemeProvider>
   )
---
+++ src/components/Switcher.tsx
@@ -0,0 +1,45 @@
+'use client'
+
+import styled from 'styled-components';
+import Switch from 'react-switch';
+import { BsSunFill, BsMoonStarsFill } from 'react-icons/bs'
+import { useTheme } from '@/contexts/ThemeContext';
+
+const ThemeSwitcher = styled.div`
+    position: absolute;
+    top: 5vh;
+    left: 87.5vw;
+    transform: scale(1.2);
+`
+
+const SunIcon = styled(BsSunFill)`
+    color: ${props => props.theme.colors.darkGrayishBlue};
+    font-size: 20px;
+    position: relative;
+    top: 3px;
+    left: 5px;
+`
+
+const MoonIcon = styled(BsMoonStarsFill)`
+    color: ${props => props.theme.colors.darkBlue};
+    font-size: 20px;
+    position: relative;
+    top: 3px;
+    left: 4px;
+`
+
+export default function Switcher() {
+    const { theme ,toggleTheme } = useTheme()
+    return(
+        <ThemeSwitcher>
+            <Switch 
+                onChange={toggleTheme}
+                checked={theme.title === "dark" && true}
+                checkedIcon={<SunIcon/>}
+                uncheckedIcon={<MoonIcon/>}
+                offColor={theme.colors.neonGreen}
+                onColor={theme.colors.neonGreen}
+            />
+        </ThemeSwitcher>
+    )
+}
